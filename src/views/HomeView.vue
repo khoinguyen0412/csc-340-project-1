1202a8b00a9e34ccaa58afad8ec820d53febdd92
khoinguyen0412 <nguyenkhoi2227@gmail.com>
2024-09-29 22:04:42 -0400

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HomePage from '../components/HomePage.vue'
-import NavBar from '../components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
+import HomePage from '@/components/HomePage.vue'
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/FooterBar.vue'
 </script>
 
 <template>
